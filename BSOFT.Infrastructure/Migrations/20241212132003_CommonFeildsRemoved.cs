3655fee7b80fd606687970958476d7b5632618cf
radha <radha.b@bannarimills.co.in>
2024-12-13 15:00:18 +0530

    Validation Behavior modified

---
+++ BSOFT.Infrastructure/Migrations/20241212132003_CommonFeildsRemoved.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BSOFT.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommonFeildsRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleName",
+                schema: "AppSecurity",
+                table: "RoleEntitlement");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByName",
+                schema: "AppSecurity",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                schema: "AppSecurity",
+                table: "RoleEntitlement",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedIP",
+                schema: "AppData",
+                table: "Department",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedByName",
+                schema: "AppData",
+                table: "Department",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedBy",
+                schema: "AppData",
+                table: "Department",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedAt",
+                schema: "AppData",
+                table: "Department",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByName",
+                schema: "AppData",
+                table: "Company",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleEntitlement_RoleId",
+                schema: "AppSecurity",
+                table: "RoleEntitlement",
+                column: "RoleId");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleEntitlement_Role_RoleId",
+                schema: "AppSecurity",
+                table: "RoleEntitlement");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RoleEntitlement_RoleId",
+                schema: "AppSecurity",
+                table: "RoleEntitlement");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                schema: "AppSecurity",
+                table: "RoleEntitlement");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByName",
+                schema: "AppSecurity",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleName",
+                schema: "AppSecurity",
+                table: "RoleEntitlement",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedIP",
+                schema: "AppData",
+                table: "Department",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedByName",
+                schema: "AppData",
+                table: "Department",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedBy",
+                schema: "AppData",
+                table: "Department",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedAt",
+                schema: "AppData",
+                table: "Department",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedByName",
+                schema: "AppData",
+                table: "Company",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
---
+++ BSOFT.Infrastructure/Migrations/20241212133459_UserRoleIdMapping.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BSOFT.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRoleIdMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                schema: "AppSecurity",
+                table: "Users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                schema: "AppSecurity",
+                table: "Users",
+                column: "RoleId");
+
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Role_RoleId",
+                schema: "AppSecurity",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_RoleId",
+                schema: "AppSecurity",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                schema: "AppSecurity",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
