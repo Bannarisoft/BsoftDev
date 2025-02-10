72c4de6fa53087916121ba5f1ef9ce2e9f21cd76
arun <arunpandiyan@bannarimills.co.in>
2025-02-10 13:39:37 +0530

    Pagination and autocomplete done for module

---
+++ UserManagement/Core/Core.Application/Modules/Queries/GetModules/ModuleAutoCompleteDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Application.Modules.Queries.GetModules
+{
+    public class ModuleAutoCompleteDTO
+    {
+        public int Id { get; set; }
+        public string ModuleName { get; set; }
+    }
+}
