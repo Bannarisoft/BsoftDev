48a44d04677d7205fed6f86281d1de7843409760
USHA <ushadevi@bannarimills.co.in>
2025-02-12 15:01:36 +0530

    Profile changes

--- FixedAssetManagement/Core/Core.Application/Common/Mappings/ManufactureProfile.cs
+++ FixedAssetManagement/Core/Core.Application/Common/Mappings/ManufactureProfile.cs
@@ -4,21 +4,6 @@ namespace Core.Application.Common.Mappings
 {
     public class ManufactureProfile : Profile
     {
-        public DepreciationGroupProfile()
-        { 
-            CreateMap<DeleteDepreciationGroupCommand, DepreciationGroups>()            
-            .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => IsDelete.Deleted));            
-            
-            CreateMap<CreateDepreciationGroupCommand, DepreciationGroups>()
-            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => Status.Active))            
-            .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => IsDelete.NotDeleted)); 
-
-            CreateMap<UpdateDepreciationGroupCommand, DepreciationGroups>()
-            .ForMember(dest => dest.IsDeleted, opt => opt.Ignore())
-            .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => IsDelete.NotDeleted));     
-             
-            CreateMap<DepreciationGroups, DepreciationGroupAutoCompleteDTO>();    
-            CreateMap<DepreciationGroups, DepreciationGroupDTO>();             
-        }
+       
     }
 }
