6ffb9cc7fd1e94f2eea2e7d339d3009cbbae21d8
Dhayal <dhayal@bannarimills.co.in>
2025-02-17 10:06:17 +0530

    tt

--- FixedAssetManagement/Core/Core.Application/Common/Mappings/MisctypeMasterProfile.cs
+++ FixedAssetManagement/Core/Core.Application/Common/Mappings/MisctypeMasterProfile.cs
@@ -27,7 +27,6 @@ namespace Core.Application.Common.Mappings
                 CreateMap<UpdateMiscTypeMasterCommand, Core.Domain.Entities.MiscTypeMaster>()
                  .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.IsActive ==1 ? Status.Active : Status.Inactive));
 
-
                 CreateMap<DeleteMiscTypeMasterCommand,  Core.Domain.Entities.MiscTypeMaster>()
                  .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => IsDelete.Deleted)); 
         }
