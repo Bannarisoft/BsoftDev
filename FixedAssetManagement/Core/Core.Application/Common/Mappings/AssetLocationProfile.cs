15ca7699e4530121700a2e058f348d0c53f86fe8
Dhayal <dhayal@bannarimills.co.in>
2025-02-25 13:17:51 +0530

    assetlocation mapping issue

--- FixedAssetManagement/Core/Core.Application/Common/Mappings/AssetLocationProfile.cs
+++ FixedAssetManagement/Core/Core.Application/Common/Mappings/AssetLocationProfile.cs
@@ -18,11 +18,21 @@ namespace Core.Application.Common.Mappings
             CreateMap<Core.Domain.Entities.AssetMaster.AssetLocation,AssetLocationDto>();
 
             CreateMap<CreateAssetLocationCommand, Core.Domain.Entities.AssetMaster.AssetLocation>();
-            CreateMap<UpdateAssetLocationCommand, Core.Domain.Entities.AssetMaster.AssetLocation>();
-
+            CreateMap<UpdateAssetLocationCommand, AssetLocationDto>() 
+            .ForMember(dest => dest.AssetId, opt => opt.Ignore())  
+            .ForMember(dest => dest.UnitId, opt => opt.MapFrom(src => src.UnitId))
+            .ForMember(dest => dest.DepartmentId, opt => opt.MapFrom(src => src.DepartmentId))                
+            .ForMember(dest => dest.LocationId, opt => opt.MapFrom(src => src.LocationId))
+            .ForMember(dest => dest.SubLocationId, opt => opt.MapFrom(src => src.SubLocationId))
+            .ForMember(dest => dest.CustodianId, opt => opt.MapFrom(src => src.CustodianId))
+            .ForMember(dest => dest.DepartmentId, opt => opt.MapFrom(src => src.DepartmentId))
+            .ForMember(dest => dest.UnitId, opt => opt.MapFrom(src => src.UnitId));
+            
             CreateMap<Core.Domain.Entities.AssetMaster.Employee,GetCustodianDto>()           
             .ForMember(dest => dest.CustodianId, opt => opt.MapFrom(src => src.Empcode))
             .ForMember(dest => dest.CustodianName, opt => opt.MapFrom(src => src.Empname));
+
+           
                 
         }
         
