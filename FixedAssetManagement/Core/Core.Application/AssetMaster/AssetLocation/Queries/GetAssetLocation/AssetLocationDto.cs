4ab0b96f6a9aa2ed692b629eaa0bd619e3996541
Dhayal <dhayal@bannarimills.co.in>
2025-03-17 17:01:21 +0530

    sub location

--- FixedAssetManagement/Core/Core.Application/AssetMaster/AssetLocation/Queries/GetAssetLocation/AssetLocationDto.cs
+++ FixedAssetManagement/Core/Core.Application/AssetMaster/AssetLocation/Queries/GetAssetLocation/AssetLocationDto.cs
@@ -7,8 +7,8 @@ namespace Core.Application.AssetLocation.Queries.GetAssetLocation
 {
     public class AssetLocationDto 
     {  
-         public int Id { get; set; }
-         public int AssetId { get; set; }
+        public int Id { get; set; }
+        public int AssetId { get; set; }
         public int UnitId { get; set; } 
         public int DepartmentId { get; set; }
         public int LocationId { get; set; }
