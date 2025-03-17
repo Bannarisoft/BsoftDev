63aa5f3fe782aa66c541a1ee6fc12eaf4592307d
USHA <ushadevi@bannarimills.co.in>
2025-03-17 14:57:48 +0530

    qrcode testing

--- FixedAssetManagement/Core/Core.Application/QRCode/GenerateQRCodeCommandHandler.cs
+++ FixedAssetManagement/Core/Core.Application/QRCode/GenerateQRCodeCommandHandler.cs
@@ -12,7 +12,9 @@ namespace Core.Application.QRCode
                 using (var qrCodeData = qrGenerator.CreateQrCode(request.Content, QRCodeGenerator.ECCLevel.Q))
                 {
                     var qrCode = new PngByteQRCode(qrCodeData); // ✅ Use PngByteQRCode
-                    return qrCode.GetGraphic(20); // ✅ Returns QR as PNG byte array
+                    //return qrCode.GetGraphic(20); // ✅ Returns QR as PNG byte array
+                    int qrSize = 300; // Adjust size here (e.g., 300x300 pixels)
+                    return qrCode.GetGraphic(qrSize);
                 }
             }
         }
