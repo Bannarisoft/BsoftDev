187f7bf42950026ee05072fec9a2eb399d27571f
radhabose <radha.b@bannarimills.co.in>
2025-03-18 17:43:08 +0530

    Maintenance Microservice included

---
+++ MaintenanceManagement/MaintenanceManagement.API/Configurations/HangfireSetup.cs
@@ -0,0 +1,19 @@
+using Hangfire;
+using MaintenanceManagement.API.Filters;
+namespace MaintenanceManagement.API.Configurations
+{
+    public static class HangfireSetup
+    {
+        public static void ConfigureHangfireDashboard(this IApplicationBuilder app)
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                DashboardTitle = "BSOFT Hangfire Dashboard",
+                Authorization = new[]
+                {
+                    new HangfireAuthorizationFilter("admin", "basml@1234") // Customize username/password
+                }
+            });
+        }
+    }
+}
