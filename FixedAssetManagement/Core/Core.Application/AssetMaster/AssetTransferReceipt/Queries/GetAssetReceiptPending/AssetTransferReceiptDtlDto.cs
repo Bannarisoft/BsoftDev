264ea78791c08852b391c69ecebf25b31918894f
praveen <praveen@bannarimills.co.in>
2025-04-09 11:13:03 +0530

    AssetReceipt AssetLocation Changes

--- FixedAssetManagement/Core/Core.Application/AssetMaster/AssetTransferReceipt/Queries/GetAssetReceiptPending/AssetTransferReceiptDtlDto.cs
+++ FixedAssetManagement/Core/Core.Application/AssetMaster/AssetTransferReceipt/Queries/GetAssetReceiptPending/AssetTransferReceiptDtlDto.cs
@@ -10,7 +10,7 @@ namespace Core.Application.AssetMaster.AssetTransferReceipt.Queries.GetAssetRece
         public int AssetId { get; set; } 
         public int? LocationId { get; set; }
         public int? SubLocationId { get; set; } 
-        public string? UserID { get; set; }
+        public int? UserID { get; set; }
         public string? UserName { get; set; }
         public byte? AckStatus { get; set; } = 0;   // Default to 0
        // public DateTimeOffset? AckDate { get; set; }
