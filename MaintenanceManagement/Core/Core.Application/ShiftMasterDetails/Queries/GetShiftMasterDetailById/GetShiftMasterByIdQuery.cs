7687b32dad08555a32b091faa7d3282283b43c9a
arun <arunpandiyan@bannarimills.co.in>
2025-04-29 16:22:45 +0530

    get all for shift details

--- MaintenanceManagement/Core/Core.Application/ShiftMasterDetails/Queries/GetShiftMasterDetailById/GetShiftMasterByIdQuery.cs
+++ MaintenanceManagement/Core/Core.Application/ShiftMasterDetails/Queries/GetShiftMasterDetailById/GetShiftMasterByIdQuery.cs
@@ -7,7 +7,7 @@ using MediatR;
 
 namespace Core.Application.ShiftMasterDetails.Queries.GetShiftMasterDetailById
 {
-    public class GetShiftMasterByIdQuery : IRequest<ApiResponseDTO<ShiftMasterDetailDTO>>
+    public class GetShiftMasterByIdQuery : IRequest<ApiResponseDTO<ShiftMasterDetailByIdDto>>
     {
         public int Id { get; set; }
     }
