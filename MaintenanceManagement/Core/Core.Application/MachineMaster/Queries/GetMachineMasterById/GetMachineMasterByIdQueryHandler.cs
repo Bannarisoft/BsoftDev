63b159674aac0863c3a109a635cff4d723628816
radhabose <radha.b@bannarimills.co.in>
2025-04-30 18:31:16 +0530

    Merge Conflicts fixed

--- MaintenanceManagement/Core/Core.Application/MachineMaster/Queries/GetMachineMasterById/GetMachineMasterByIdQueryHandler.cs
+++ MaintenanceManagement/Core/Core.Application/MachineMaster/Queries/GetMachineMasterById/GetMachineMasterByIdQueryHandler.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
+using Contracts.Interfaces.External.IUser;
 using Core.Application.Common.HttpResponse;
-using Core.Application.Common.Interfaces.External.IDepartment;
 using Core.Application.Common.Interfaces.IMachineMaster;
-using Core.Application.Common.Interfaces.IMaintenanceCategory;
 using Core.Application.MachineMaster.Queries.GetMachineMaster;
 using Core.Domain.Events;
 using MediatR;
@@ -19,15 +14,15 @@ namespace Core.Application.MachineMaster.Queries.GetMachineMasterById
         private readonly IMachineMasterQueryRepository _imachineMasterQueryRepository;        
         private readonly IMapper _mapper;
         private readonly IMediator _mediator;
-         private readonly IDepartmentService _departmentService;
+        private readonly IDepartmentGrpcClient _departmentGrpcClient; // ✅ Interface, not DepartmentServiceClient
 
 
-        public GetMachineMasterByIdQueryHandler(IMachineMasterQueryRepository imachineMasterQueryRepository, IMapper mapper, IMediator mediator , IDepartmentService departmentService)
+        public GetMachineMasterByIdQueryHandler(IMachineMasterQueryRepository imachineMasterQueryRepository, IMapper mapper, IMediator mediator , IDepartmentGrpcClient departmentGrpcClient)
         {
             _imachineMasterQueryRepository = imachineMasterQueryRepository;            
             _mapper = mapper;
             _mediator = mediator;
-            _departmentService = departmentService;
+            _departmentGrpcClient = departmentGrpcClient;
         }
 
         public async Task<ApiResponseDTO<MachineMasterDto>> Handle(GetMachineMasterByIdQuery request, CancellationToken cancellationToken)
@@ -40,7 +35,7 @@ namespace Core.Application.MachineMaster.Queries.GetMachineMasterById
             }
             // Map a single entity
             var machineMaster = _mapper.Map<MachineMasterDto>(result);
-            var departments = await _departmentService.GetAllDepartmentAsync();
+            var departments = await _departmentGrpcClient.GetAllDepartmentsAsync();
                 var dept = departments.FirstOrDefault(d => d.DepartmentId == machineMaster.DepartmentId);
                 if (dept != null)
                 {
