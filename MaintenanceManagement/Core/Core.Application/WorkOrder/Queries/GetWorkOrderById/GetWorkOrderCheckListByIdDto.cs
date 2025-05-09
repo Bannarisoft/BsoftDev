a225929346468f6be1a9cb7fbe412604a88ec290
USHA <ushadevi@bannarimills.co.in>
2025-05-09 13:10:24 +0530

    WO API

--- MaintenanceManagement/Core/Core.Application/WorkOrder/Queries/GetWorkOrderById/GetWorkOrderCheckListByIdDto.cs
+++ MaintenanceManagement/Core/Core.Application/WorkOrder/Queries/GetWorkOrderById/GetWorkOrderCheckListByIdDto.cs
@@ -4,7 +4,7 @@ namespace Core.Application.WorkOrder.Queries.GetWorkOrderById
     public class GetWorkOrderCheckListByIdDto
     {
         public int CheckListId { get; set; }
-        public string? ActivityCheckList { get; set; }       
+        public string? ActivityChecklist { get; set; }       
         public byte IsCompleted { get; set; }      
         public int ActivityID { get; set; }    
         public string? ActivityName { get; set; }
