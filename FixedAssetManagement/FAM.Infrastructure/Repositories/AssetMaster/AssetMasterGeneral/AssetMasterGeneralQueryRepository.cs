1910beee091aff853a4b093e5a800df8e0468f4f
BASML <veeraraj@bannarimill.com>
2025-05-11 14:02:34 +0530

    API

--- FixedAssetManagement/FAM.Infrastructure/Repositories/AssetMaster/AssetMasterGeneral/AssetMasterGeneralQueryRepository.cs
+++ FixedAssetManagement/FAM.Infrastructure/Repositories/AssetMaster/AssetMasterGeneral/AssetMasterGeneralQueryRepository.cs
@@ -290,7 +290,7 @@ namespace FAM.Infrastructure.Repositories.AssetMaster.AssetMasterGeneral
                 FROM [FixedAsset].[AssetPurchaseDetails] AP
                 LEFT JOIN [Bannari].[AppData].[Unit] U ON AP.OldUnitId = U.OldUnitId
                 INNER JOIN [FixedAsset].[AssetSource] ASource ON ASource.Id=AP.AssetSourceId
-                WHERE U.UnitId = @UnitId AND AP.AssetId = @AssetId;
+                WHERE U.Id = @UnitId AND AP.AssetId = @AssetId;
 
                 SELECT A.Id,SM.SpecificationName,A.SpecificationValue,A.SpecificationId,SM.IsDefault FROM  [FixedAsset].[AssetSpecifications] A
                 INNER JOIN [FixedAsset].[SpecificationMaster] SM ON SM.Id=A.SpecificationId
@@ -440,7 +440,7 @@ namespace FAM.Infrastructure.Repositories.AssetMaster.AssetMasterGeneral
                 FROM [FixedAsset].[AssetPurchaseDetails] AP
                 LEFT JOIN [Bannari].[AppData].[Unit] U ON AP.OldUnitId = U.OldUnitId
                 INNER JOIN [FixedAsset].[AssetSource] ASource ON ASource.Id=AP.AssetSourceId
-                WHERE U.UnitId = @UnitId AND AP.AssetId = @AssetId;             
+                WHERE U.Id = @UnitId AND AP.AssetId = @AssetId;             
 
                 SELECT AC.Id,AssetSourceId,Amount,JournalNo,CostType,MM.Code CostTypeDesc
                 FROM [FixedAsset].[AssetAdditionalCost]AC
