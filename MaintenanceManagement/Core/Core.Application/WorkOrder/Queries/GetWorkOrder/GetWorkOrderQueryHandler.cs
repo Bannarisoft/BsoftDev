5980270330b3c3bc15091d930d417072baef0602
USHA <ushadevi@bannarimills.co.in>
2025-05-24 09:47:21 +0530

    Department datacontrol in Maintenance

--- MaintenanceManagement/Core/Core.Application/WorkOrder/Queries/GetWorkOrder/GetWorkOrderQueryHandler.cs
+++ MaintenanceManagement/Core/Core.Application/WorkOrder/Queries/GetWorkOrder/GetWorkOrderQueryHandler.cs
@@ -1,5 +1,6 @@
 
 using AutoMapper;
+using Contracts.Interfaces.External.IUser;
 using Core.Application.Common.HttpResponse;
 using Core.Application.Common.Interfaces.IWorkOrder;
 using Core.Application.WorkOrder.Queries.GetWorkOrderById;
@@ -13,18 +14,38 @@ namespace Core.Application.WorkOrder.Queries.GetWorkOrder
         private readonly IWorkOrderQueryRepository _workOrderRepository;
         private readonly IMapper _mapper;
         private readonly IMediator _mediator; 
+        private readonly IDepartmentGrpcClient _departmentGrpcClient;
 
-        public GetWorkOrderQueryHandler(IWorkOrderQueryRepository workOrderRepository , IMapper mapper, IMediator mediator)
+        public GetWorkOrderQueryHandler(IWorkOrderQueryRepository workOrderRepository , IMapper mapper, IMediator mediator, IDepartmentGrpcClient departmentGrpcClient)
         {
             _workOrderRepository = workOrderRepository;
             _mapper = mapper;
             _mediator = mediator;
+            _departmentGrpcClient = departmentGrpcClient;
         }
   
         public async Task<ApiResponseDTO<List<Dictionary<string, List<GetWorkOrderDto>>>>> Handle(GetWorkOrderQuery request, CancellationToken cancellationToken)
         {
            var workOrder = await _workOrderRepository.GetAllWOAsync(request.fromDate,request.toDate, request.requestTypeId, request.departmentId);            
            var mappedWorkOrders = _mapper.Map<List<GetWorkOrderDto>>(workOrder);
+
+             // 🔥 Fetch departments using gRPC
+            var departments = await _departmentGrpcClient.GetAllDepartmentAsync(); // ✅ Clean call
+
+            // var departments = departmentResponse.Departments.ToList();
+            var departmentLookup = departments.ToDictionary(d => d.DepartmentId, d => d.DepartmentName);
+
+            var PreventiveSchedulerDictionary = new Dictionary<int, GetWorkOrderDto>();
+            // 🔥 Map department names to preventiveScheduler
+            foreach (var data in mappedWorkOrders)
+            {
+                if (departmentLookup.TryGetValue(data.DepartmentId, out var departmentName) && departmentName != null)
+                {
+                    data.Department = departmentName;
+                }
+                PreventiveSchedulerDictionary[data.DepartmentId] = data;
+            }
+
             var groupedWorkOrders = mappedWorkOrders
             .GroupBy(w => w.MaintenanceType ?? "Unknown")
             .Select(g => new Dictionary<string, List<GetWorkOrderDto>>
