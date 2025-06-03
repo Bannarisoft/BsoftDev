33ed0d614c63bc5a7d70a25f4921f2bcb42d337d
Dhayal <dhayal@bannarimills.co.in>
2025-06-03 13:01:31 +0530

    FeederGroup implementation

---
+++ MaintenanceManagement/Core/Core.Application/Power/FeederGroup/Queries/GetFeederGroup/GetFeederGroup.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Application.Power.FeederGroup.Queries.GetFeederGroup
+{
+    public class GetFeederGroup
+    {
+        
+    }
+}
---
+++ MaintenanceManagement/MaintenanceManagement.API/Controllers/Power/FeederGroupController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MaintenanceManagement.API.Controllers.Power
+{
+    [Route("[controller]")]
+    public class FeederGroupController : Controller
+    {
+        private readonly ILogger<FeederGroupController> _logger;
+
+        public FeederGroupController(ILogger<FeederGroupController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
