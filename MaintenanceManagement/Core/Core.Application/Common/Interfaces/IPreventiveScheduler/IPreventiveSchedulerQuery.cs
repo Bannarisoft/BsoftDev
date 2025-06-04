40bd42ea4ba1f944d43acb3461257248a60cc67c
USHA <ushadevi@bannarimills.co.in>
2025-06-04 14:21:51 +0530

    work order done issue. last maintenance date issue so query changed.

--- MaintenanceManagement/Core/Core.Application/Common/Interfaces/IPreventiveScheduler/IPreventiveSchedulerQuery.cs
+++ MaintenanceManagement/Core/Core.Application/Common/Interfaces/IPreventiveScheduler/IPreventiveSchedulerQuery.cs
@@ -16,7 +16,7 @@ namespace Core.Application.Common.Interfaces.IPreventiveScheduler
         Task<bool> NotFoundAsync(int id);
         Task<(DateTime nextDate, DateTime reminderDate)> CalculateNextScheduleDate(DateTime startDate, int interval, string unit, int reminderDays);
         Task<List<PreventiveSchedulerDetail>> GetPreventiveSchedulerDetail(int PreventiveSchedulerId);
-        Task<DateTimeOffset?> GetLastMaintenanceDateAsync(int machineId, int PreventiveSchedulerId, string miscType, string misccode);
+        Task<DateTimeOffset?> GetLastMaintenanceDateAsync(int machineId, int PreventiveDetailId, string miscType, string misccode);
         Task<PreventiveSchedulerDetail> GetPreventiveSchedulerDetailById(int Id);
         Task<bool> UpdateValidation(int id);
         Task<IEnumerable<dynamic>> GetAbstractSchedulerByDate(int DepartmentId);
