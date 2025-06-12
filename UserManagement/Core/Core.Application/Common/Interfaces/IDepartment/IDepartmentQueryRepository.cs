7e88196711ab26e99cc9a2d8ce71931d82eff612
arun <arunpandiyan@bannarimills.co.in>
2025-06-12 16:15:36 +0530

    new api for maintenance department control user rights based

--- UserManagement/Core/Core.Application/Common/Interfaces/IDepartment/IDepartmentQueryRepository.cs
+++ UserManagement/Core/Core.Application/Common/Interfaces/IDepartment/IDepartmentQueryRepository.cs
@@ -1,3 +1,4 @@
+using Core.Application.Departments.Queries.GetDepartmentByGroupWithControl;
 using Core.Application.Departments.Queries.GetDepartments;
 using Core.Domain.Entities;
 using System;
@@ -15,12 +16,13 @@ namespace Core.Application.Common.Interfaces.IDepartment
         Task<(List<DepartmentDto>, int)> GetAllDepartmentAsync(int PageNumber, int PageSize, string? SearchTerm);
 
         Task<Department> GetByIdAsync(int id);
-        Task<List<Department>> GetAllDepartmentAutoCompleteSearchAsync(string SearchDept);      
-        
+        Task<List<Department>> GetAllDepartmentAutoCompleteSearchAsync(string SearchDept);
+
         Task<bool> FKColumnExistValidation(int Id);
         Task<List<Department>> GetDepartment_SuperAdmin(string SearchDept);
 
         Task<List<DepartmentWithGroupDto>> GetDepartmentsByDepartmentGroupIdAsync(string departmentGroupName);
+        Task<List<DepartmentWithControlByGroupDto>> GetDepartmentsByDepartmentGroupWithControl(string departmentGroupName);
          
         
 
