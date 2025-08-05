0ea6870a8adb0dbb2c08fe63d461683587d492c8
arun <arunpandiyan@bannarimills.co.in>
2025-08-05 16:25:11 +0530

    purchase indent

---
+++ PurchaseManagement/Core/Core.Domain/Entities/IndentDetail.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Domain.Common;
+
+namespace Core.Domain.Entities
+{
+    public class IndentDetail : BaseEntity
+    {
+        public int IndentHeaderId { get; set; }
+        public int ItemId { get; set; }
+        public decimal QuantityRequired { get; set; }
+        public DateOnly RequiredDate { get; set; }
+        public decimal TotalEstimatedCost { get; set; }
+        public int PRConsumptionDays { get; set; }
+        public string Remark { get; set; }
+        public IndentHeader IndentHeader { get; set; }
+        
+    }
+}
