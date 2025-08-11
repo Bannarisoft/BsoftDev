2d10239ef0e8497bf91703ba51caa7e868f813a8
praveen <praveen@bannarimills.co.in>
2025-08-11 13:45:37 +0530

    Party Group Changes PartyMaster Table Migration

---
+++ PartyManagement/PartyManagement.Infrastructure/Data/Configurations/PartyAddressConfiguration.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PartyManagement.Infrastructure.Data.Configurations
+{
+    public class PartyAddressConfiguration : IEntityTypeConfiguration<PartyAddress>
+    {
+        public void Configure(EntityTypeBuilder<PartyAddress> builder)
+        {
+            builder.ToTable("PartyAddress", "Party");
+            // Primary Key
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.Id)
+                .HasColumnName("Id")
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.Property(m => m.PartyId)  // Foreign Key column
+               .HasColumnName("PartyId")
+               .HasColumnType("int")  // Set as int
+               .IsRequired();
+
+            // Foreign Key Relationship
+            builder.HasOne(m => m.PartyAddressId)
+                .WithMany(t => t.PartyAddressTypes)
+                .HasForeignKey(m => m.PartyId) // Foreign Key property in PartyContact
+                .OnDelete(DeleteBehavior.Restrict); // Use .Cascade if needed
+
+            builder.Property(m => m.AddressType)
+               .HasColumnName("AddressType")
+               .HasColumnType("nvarchar(25)");
+
+            builder.Property(m => m.AddressLine1)
+               .HasColumnName("AddressLine1")
+               .HasColumnType("nvarchar(100)");
+
+            builder.Property(m => m.AddressLine2)
+               .HasColumnName("AddressLine2")
+               .HasColumnType("nvarchar(100)");
+
+            builder.Property(m => m.City)
+               .HasColumnName("City")
+               .HasColumnType("nvarchar(50)");
+
+            builder.Property(m => m.State)
+               .HasColumnName("AddressLine4")
+               .HasColumnType("nvarchar(50)");
+
+            builder.Property(m => m.PostalCode)
+               .HasColumnName("AddressLine5")
+               .HasColumnType("nvarchar(10)");
+
+            builder.Property(m => m.Country)
+               .HasColumnName("AddressLine6")
+               .HasColumnType("nvarchar(50)");  
+        }
+    }
+}
