fa283e55949c588a3fc06e5b1caf20a162127591
arun <arunpandiyan@bannarimills.co.in>
2025-08-12 17:32:31 +0530

    status code issue

--- FixedAssetManagement/FAM.API/Controllers/AssetMaster/AssetTransferController.cs
+++ FixedAssetManagement/FAM.API/Controllers/AssetMaster/AssetTransferController.cs
@@ -69,7 +69,10 @@ namespace FAM.API.Controllers.AssetMaster
             {
                 return NotFound($"Asset Transfer with ID {id} not found.");
             }
-            return Ok(result);
+            return Ok(new {
+                StatusCode = StatusCodes.Status200OK,
+                Data = result
+            });
         }
 
 
@@ -240,7 +243,11 @@ namespace FAM.API.Controllers.AssetMaster
 
             var response = await Mediator.Send(query);
 
-            return Ok(response);
+            return Ok(new 
+            {
+                StatusCode = StatusCodes.Status200OK,
+                response
+            });
         }
 
         [HttpGet("GetCategoriesByCustodian")]
